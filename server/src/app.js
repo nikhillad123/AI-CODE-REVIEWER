9675d76972f2746b67f143a4cc911fd6189803f9
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-16 19:48:20 +0530

    Created fronted and finished frontend.

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import aiRoutes from './routes/ai.routes.js';
+import express from 'express'
+import aiRoutes from './routes/ai.routes.js'
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Mount your routes
 app.use('/api/ai', aiRoutes);
